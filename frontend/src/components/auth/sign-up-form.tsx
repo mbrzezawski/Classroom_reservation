8f65bc24153c66f405ae09da7ec5a318fccc340d
Rafał Szepieniec <rszepieniec@student.agh.edu.pl>
2025-06-17 03:05:27 +0200

    registration, allers, ...

--- frontend/src/components/auth/sign-up-form.tsx
+++ frontend/src/components/auth/sign-up-form.tsx
@@ -1,89 +1,195 @@
-import type { FC } from "react";
+import {type FC, useEffect, useState} from "react";
 import InputTextBox from "../ui/input-textbox";
 import AtIcon from "../icons/at";
 import PasswordTextBox from "./password-textbox";
 import { FormProvider, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import {data} from "autoprefixer";
+import {API_URL} from "../../api.ts";
 type SignUpFormValues = {
   email: string;
   password: string;
   repeatedPassword: string;
+  name: string;
+  surname: string;
+  specialCode: string;
 };
 
 const SignUpForm: FC = () => {
   const methods = useForm({
-    defaultValues: { email: "", password: "", repeatedPassword: "" },
+    defaultValues: { email: "", password: "", repeatedPassword: "", name: "", surname: "", specialCode: ""},
   });
-  const onSubmit = (data: SignUpFormValues) => {
+
+
+  const onSubmit = async (data: SignUpFormValues) => {
     console.log("Form submitted:", data);
+    const adminCode = "ADMIN_CODE";
+    const deansCode = "DEANS_CODE";
+    const body = {
+      email: data.email,
+      password: data.password,
+      name: data.name,
+      surname: data.surname,
+      adminCode: data.specialCode === adminCode ? adminCode : "",
+      deansCode: data.specialCode === deansCode ? deansCode : "",
+    }
+    try {
+      const response = await fetch(`${API_URL}/auth/register`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Błąd podczas rejestracji");
+      }
+
+      navigate("/login");
+    } catch (err: any) {
+      console.error("Błąd przy rejestracji:", err.message);
+      alert(err.message);
+    }
+
   };
+
+
   const {
     register,
     handleSubmit,
     watch,
+    setValue,
     formState: { errors },
   } = methods;
   const password = watch("password");
   const navigate = useNavigate();
+  const [showCodeInput, setShowCodeInput] = useState(false);
+
+
+  useEffect(() => {
+    if (showCodeInput == false) {
+      setValue("specialCode", "");
+    }
+  }, [showCodeInput]);
+
   return (
-    <FormProvider {...methods}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col border px-6 py-8 gap-[30px] rounded-[8px] "
-      >
-        <h2 className="text-[42px] text-center">Sign up</h2>
-        <InputTextBox
-          label="Email"
-          type="email"
-          placeholder="Type your email"
-          icon={<AtIcon />}
-          error={errors.email?.message}
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Invalid email format",
-            },
-          })}
-        />
-        <PasswordTextBox
-          error={errors.password?.message}
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 6,
-              message: "Password must be at least 6 characters long",
-            },
-          })}
-        />
-        <PasswordTextBox
-          confirmPassword={true}
-          error={errors.repeatedPassword?.message}
-          {...register("repeatedPassword", {
-            required: "Please confirm your password",
-            validate: (value) => value === password || "Passwords do not match",
-          })}
-        />
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="btn w-[274px] py-2 border rounded-[12px]"
-          >
-            Sign up
-          </button>
-        </div>
-        <div className="flex flex-col justify-center">
-          <p className="text-[12px] text-center">Already have an account?</p>
-          <button
-            type="button"
-            className="flex self-center btn-link no-underline cursor-pointer text-[12px] font-bold hover:underline"
-            onClick={() => navigate(`/login`)}
-          >
-            Login
-          </button>
-        </div>
-      </form>
-    </FormProvider>
+      <FormProvider {...methods}>
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col border px-6 py-8 gap-8 rounded-[8px] w-full max-w-4xl mx-auto"
+        >
+          <h2 className="text-[42px] text-center">Sign up</h2>
+
+
+          <div className="flex flex-row gap-6">
+
+            <div className="flex flex-col gap-4 flex-1">
+              <InputTextBox
+                  label="Imię"
+                  type="text"
+                  placeholder="Napisz swoje imię"
+                  icon={undefined}
+                  error={errors.name?.message}
+                  {...register("name", {
+                    required: "Imię jest wymagane",
+                  })}
+              />
+              <InputTextBox
+                  label="Nazwisko"
+                  type="text"
+                  placeholder="Napisz swoje nazwisko"
+                  icon={undefined}
+                  error={errors.surname?.message}
+                  {...register("surname", {
+                    required: "Nazwisko jest wymagane",
+                  })}
+              />
+              <InputTextBox
+                  label="Email"
+                  type="email"
+                  placeholder="Wpisz email"
+                  icon={<AtIcon/>}
+                  error={errors.email?.message}
+                  {...register("email", {
+                    required: "Email jest wymagany",
+                    pattern: {
+                      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                      message: "Niepoprawny format emaila",
+                    },
+                  })}
+              />
+            </div>
+
+            {/* Prawa kolumna */}
+            <div className="flex flex-col gap-4 flex-1">
+              <PasswordTextBox
+                  error={errors.password?.message}
+                  {...register("password", {
+                    required: "Hasło jest wymagane",
+                    minLength: {
+                      value: 6,
+                      message: "Hasło musi mieć co najmniej 6 znaków",
+                    },
+                  })}
+              />
+              <PasswordTextBox
+                  confirmPassword={true}
+                  error={errors.repeatedPassword?.message}
+                  {...register("repeatedPassword", {
+                    required: "Powtórz hasło",
+                    validate: (value) =>
+                        value === password || "Hasła nie są takie same",
+                  })}
+              />
+
+              <div className="flex flex-row gap-4 flex-1">
+              <label className = "text-sm flex items-center gap-2 mt-2">
+                <input
+                    type="checkbox"
+                    className="checkbox"
+                    onChange={(e) => setShowCodeInput(e.target.checked)}
+                    checked={showCodeInput}
+                />
+                Mam kod specjalny
+              </label>
+
+              {showCodeInput && (
+                  <InputTextBox
+                      label={""} icon={undefined}
+                      type="code"
+                      placeholder="Wpisz kod specjalny"
+                      error={errors.specialCode?.message}
+                      {...register("specialCode")}
+                  />
+              )}
+
+              </div>
+            </div>
+          </div>
+
+          {/* Przycisk */}
+          <div className="flex justify-center mt-6">
+            <button
+                type="submit"
+                className="btn w-[274px] py-2 border rounded-[12px]"
+            >
+              Sign up
+            </button>
+          </div>
+
+          {/* Link do logowania */}
+          <div className="flex flex-col justify-center">
+            <p className="text-[12px] text-center">Masz już konto?</p>
+            <button
+                type="button"
+                className="flex self-center btn-link no-underline cursor-pointer text-[12px] font-bold hover:underline"
+                onClick={() => navigate(`/login`)}
+            >
+              Zaloguj się
+            </button>
+          </div>
+        </form>
+
+      </FormProvider>
   );
 };
 export default SignUpForm;
--- frontend/src/hooks/useSoftwareService.ts
+++ frontend/src/hooks/useSoftwareService.ts
@@ -5,7 +5,11 @@ import {API_URL} from "../api.ts";
 export const useSoftwareService = () => {
     const createSoftware = useMutation({
         mutationFn: async (name: string) => {
-            const response = await axios.post(API_URL + "/software", { name });
+            const response = await axios.post(API_URL + "/software", { name }, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
             console.log("response: ", response);
             return response.data;
         },
