8140c50e21de064381bf9954b94fc728488fb22e
mbrzezawski <mbrzezawski3@gmail.com>
2025-06-20 15:27:00 +0200

    Front hosting

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,3 @@
 // export const API_URL = "https://demo-deployment-latest-1-u69y.onrender.com"
 
-export const API_URL = "http://localhost:8080"
+export const API_URL = import.meta.env.VITE_API_URL;
